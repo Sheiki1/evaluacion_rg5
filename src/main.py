564f3da1bd4fdb69c549dd42cc2b0b4780019d95
Sheiki <60660345+Sheiki1@users.noreply.github.com>
2020-11-27 18:38:23 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,9 +1,3 @@
-
-import csv
-from flask import Flask, jsonify
-
-app = Flask(__name__,static_url_path="")
-
 import csv
 from flask import Flask, jsonify
 
@@ -23,4 +17,4 @@ def convert_csv_to_json():
         return s
 
 if __name__== '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
